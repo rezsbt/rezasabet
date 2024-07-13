5a04a22b673c2f2902b89b3684f70dc81ae5bcf4
rezsbt <rezsabet24@gmail.com>
2024-07-13 20:14:36 +0330

    Update master

--- src/lib/providers/Providers.tsx
+++ src/lib/providers/Providers.tsx
@@ -3,7 +3,16 @@
 import { ChildrenProps } from '@/utils/types.util'
 import { NextUIProvider } from '@nextui-org/react'
 
+import dynamic from 'next/dynamic'
+const NextThemesProvider = dynamic(() => import('@/lib/providers/NextThemesProvider'), { ssr: false })
+
 const Providers = ({ children }: ChildrenProps) => {
-  return <NextUIProvider>{children}</NextUIProvider>
+  return (
+    <NextUIProvider>
+      <NextThemesProvider defaultTheme='dark' attribute='class'>
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  )
 }
 export default Providers
