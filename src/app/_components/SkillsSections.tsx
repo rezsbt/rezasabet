6a909ea24248b2ad55f691e19e9867aae5e44304
rezsbt <rezsabet24@gmail.com>
2024-07-14 20:33:42 +0330

    Skills section completed

--- src/app/_components/SkillsSections.tsx
+++ src/app/_components/SkillsSections.tsx
@@ -1,37 +1,171 @@
 import SectionTitle from '@/components/shared/SectionTitle'
 import { Card, CardBody, CardHeader, Divider, Progress } from '@nextui-org/react'
 import React from 'react'
+import { uuid } from 'uuidv4'
 
 // ** Types
 type SkillProps = {
-  title: string
-  percentage: number
+  name: string
+  skillLevel: number
 }
+type SkillGroupProps = {
+  children: React.ReactNode
+  title: string
+}
+type SkillType = {
+  name: string
+  skillLevel: number
+}
+type DataType = {
+  title: string
+  skills: SkillType[]
+}
+
+// ** Data
+const data: DataType[] = [
+  {
+    title: 'Foundamentals',
+    skills: [
+      {
+        name: 'HTML/CSS/JS',
+        skillLevel: 90
+      },
+      {
+        name: 'TypeScript',
+        skillLevel: 90
+      },
+      {
+        name: 'NodeJS',
+        skillLevel: 90
+      },
+      {
+        name: 'SCSS/SASS',
+        skillLevel: 90
+      }
+    ]
+  },
+  {
+    title: 'Web Development',
+    skills: [
+      {
+        name: 'ReactJS',
+        skillLevel: 90
+      },
+      {
+        name: 'NextJS',
+        skillLevel: 90
+      },
+      {
+        name: 'Redux/RTK',
+        skillLevel: 70
+      },
+      {
+        name: 'Zustand',
+        skillLevel: 80
+      }
+    ]
+  },
+  {
+    title: 'Ui Libraries',
+    skills: [
+      {
+        name: 'TailWind',
+        skillLevel: 90
+      },
+      {
+        name: 'Ant Design',
+        skillLevel: 70
+      },
+      {
+        name: 'MUI',
+        skillLevel: 70
+      },
+      {
+        name: 'Bootstrap',
+        skillLevel: 70
+      },
+      {
+        name: 'NextUI',
+        skillLevel: 90
+      }
+    ]
+  },
+  {
+    title: 'Graphic Tools',
+    skills: [
+      {
+        name: 'Adobe Photoshop',
+        skillLevel: 40
+      },
+      {
+        name: 'Adobe XD',
+        skillLevel: 40
+      },
+      {
+        name: 'Figma',
+        skillLevel: 60
+      }
+    ]
+  },
+  {
+    title: 'Other',
+    skills: [
+      {
+        name: 'Python',
+        skillLevel: 50
+      },
+      {
+        name: 'Linux',
+        skillLevel: 40
+      },
+      {
+        name: 'Git/GitHub',
+        skillLevel: 80
+      },
+      {
+        name: 'WordPress',
+        skillLevel: 50
+      }
+    ]
+  }
+]
 
 const SkillsSections = () => {
   return (
     <section className='bg-background-200 py-header'>
       <div className='container'>
         <SectionTitle>Skills</SectionTitle>
-        <div className='flex flex-col gap-6'>
-          <Skill title='HTML/CSS' percentage={90} />
-          <Skill title='JavaScript/TypeScript' percentage={90} />
-          <Skill title='React.JS' percentage={90} />
-          <Skill title='Next.JS' percentage={90} />
+        <div className='grid grid-cols-1 gap-5 sm:grid-cols-2'>
+          {data.map((group: DataType) => (
+            <SkillGroup title={group.title} key={uuid()}>
+              {group.skills.map((item: SkillType) => (
+                <Skill key={uuid()} name={item.name} skillLevel={item.skillLevel} />
+              ))}
+            </SkillGroup>
+          ))}
         </div>
       </div>
     </section>
   )
 }
 
-const Skill = ({ title, percentage }: SkillProps) => (
+const Skill = ({ name, skillLevel }: SkillProps) => (
   <div>
     <div className='mb-1 flex items-center justify-between text-sm'>
-      <span>{title}</span>
-      <span>{percentage}%</span>
+      <span>{name}</span>
+      <span>{skillLevel}%</span>
     </div>
-    <Progress value={percentage} size='sm' color='secondary' />
+    <Progress value={skillLevel} size='sm' color='secondary' />
   </div>
+)
+
+const SkillGroup = ({ children, title }: SkillGroupProps) => (
+  <Card className='p-3'>
+    <CardHeader>
+      <h4>{title}</h4>
+    </CardHeader>
+    <CardBody className='space-y-4'>{children}</CardBody>
+  </Card>
 )
 
 export default SkillsSections
