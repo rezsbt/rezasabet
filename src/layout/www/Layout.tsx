865be1cd45ffdecc0e25ac73bb51cd94b873fe82
rezsbt <rezsabet24@gmail.com>
2024-08-04 11:44:06 +0330

    Update www layout

--- src/layout/www/Layout.tsx
+++ src/layout/www/Layout.tsx
@@ -3,18 +3,13 @@
 import Footer from '@/layout/www/components/Footer'
 import Intro from '@/layout/www/components/Intro'
 import { ChildrenProps } from '@/utils/types.util'
-import classNames from 'classnames'
-import { useState } from 'react'
 
 const WwwLayout = ({ children }: ChildrenProps) => {
-  // ** States
-  const [showIntro, setShowIntro] = useState<boolean>(true)
-
   return (
-    <div className={classNames({ 'overflow-hidden': showIntro })}>
+    <div>
       <main>{children}</main>
       <Footer />
-      <Intro setShowIntro={setShowIntro} showIntro={showIntro} />
+      <Intro />
     </div>
   )
 }
--- src/layout/www/components/Intro.tsx
+++ src/layout/www/components/Intro.tsx
@@ -1,43 +1,30 @@
 'use client'
 
-import Logo from '@/components/shared/Logo'
 import classNames from 'classnames'
 import { motion, Transition, Variants } from 'framer-motion'
+import { useState } from 'react'
 
-// ** Types
-type Props = {
-  setShowIntro: (prev: boolean) => void
-  showIntro: boolean
-}
-
-// ** Animation
-// const logoTransition: Transition = {
-//   duration: 0.75,
-//   delay: 0.25,
-//   ease: 'easeOut'
-// }
 const containerTransition: Transition = {
-  duration: 0.5,
+  duration: 1,
   delay: 0.5,
   ease: 'easeOut'
 }
 
-const Intro = ({ setShowIntro, showIntro }: Props) => {
+const Intro = () => {
+  // ** States
+  const [isCompleted, setIsCompleted] = useState<boolean>(true)
+
   return (
     <motion.section
       initial={{ opacity: 1 }}
       animate={{ opacity: 0 }}
       transition={containerTransition}
-      onAnimationComplete={() => setShowIntro(false)}
+      onAnimationComplete={() => setIsCompleted(false)}
       className={classNames(
         'flex-center left-0 top-0 h-screen w-full bg-background-200',
-        showIntro ? 'fixed' : '!hidden'
+        isCompleted ? 'fixed' : '!hidden'
       )}
-    >
-      {/* <motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={logoTransition}>
-        <Logo size='xl' />
-      </motion.span> */}
-    </motion.section>
+    />
   )
 }
 export default Intro
